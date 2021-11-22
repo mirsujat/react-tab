e23ca8ceb2fa829652faed0047ad47dfd7e9fe8b
Mir Sujat <mir.sujat@gmail.com>
2021-11-22 21:21:46 +0600

    mir

--- src/App.js
+++ src/App.js
@@ -15,13 +15,41 @@ function App() {
       </header>
       <Tabs>
         <Tab title="tab one">
-          <p> Panel One</p>
-        </Tab>
-         <Tab title="tab two">
-          <p> Panel Two</p>
+         <h2>Declarative</h2>
+      <p>Declarative views make your code more predictable and easier to debug.</p>
+      
+      <h2>Component-Based</h2>
+      <p>Build encapsulated components that manage their own state, then compose them to make complex UIs.</p>
+      
+      <h2>Learn Once, Write Anywhere</h2>
+      <p>We don't make assumptions about the rest of your technology stack, so you can develop new features in React without rewriting existing code.</p>
+    </Tab>
+    <Tab title="tab two">
+      <p> Panel Two</p>
+      <h2>Approachable</h2>
+      <p>Already know HTML, CSS and JavaScript? Read the guide and start building things in no time!</p>
+
+      <h2>Versatile</h2>
+      <p>Simple, minimal core with an incrementally adoptable stack that can handle apps of any scale.</p>
+
+      <h2>Performant</h2>
+      <p>16kb min+gzip Runtime<br/>
+         Blazing Fast Virtual DOM<br/>
+         Minimal Optimization Efforts</p>
         </Tab>
          <Tab title="tab three">
           <p> Panel Three</p>
+           <h2>Light-weight</h2>
+      <p>Only 7.8 kB gzipped, no dependencies<br/>
+        Small API, small learning curve</p>
+
+      <h2>Robust</h2>
+      <p>Safe-by-default templates<br/>
+        Hierarchical MVC via components</p>
+      
+      <h2>Fast</h2>
+      <p>Virtual DOM diffing and compilable templates<br/>
+        Intelligent auto-redrawing system</p>
         </Tab>
       </Tabs>
     </div>
--- src/components/Tabs/Tab/Tab.js
+++ src/components/Tabs/Tab/Tab.js
@@ -3,9 +3,9 @@ import React from 'react';
 const Tab = ({children, index, isSelected}) => (
   <div className="tabpanel" 
   id={`tabpanel_${index}`} 
-  name={`tabpanel_{index}`} 
+  name={`tabpanel_${index}`} 
   role="tabpanel" 
-  aria-labelledby={`tab_{index}`} 
+  aria-labelledby={`tab_${index}`} 
   aria-hidden={!isSelected()}
   key={`tabpanel_${index}`}
   >
